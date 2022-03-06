84601d425a605d3f536eaec979a7cda095d4d09f
Victor <Vmalho393@west-mec.edu>
2022-03-06 01:25:01 -0800

    Add final js to loader and never touch loader again

--- js/loader.js
+++ js/loader.js
@@ -1,8 +1,8 @@
-// $(window).on('load', function() {
-//     var hideScrollBar = setTimeout(function() {
-//         $('body').css({
-//             'overflow': 'auto'
-//         })
-//     }, 8500);
-//     $('#loaderContainer').delay(6000).fadeOut(3000);
-// })
+$(window).on('load', function() {
+    var hideScrollBar = setTimeout(function() {
+        $('body').css({
+            'overflow': 'auto'
+        })
+    }, 8500);
+    $('#loaderContainer').delay(6000).fadeOut(3000);
+})
